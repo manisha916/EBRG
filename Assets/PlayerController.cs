bec319822da132829d6e264c5aab6d8d6c324d75
yudiz_shiv <pradeep.palo@yudiz.com>
2023-06-30 19:27:43 +0530

    Ball jumping & double jumping.

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -5,61 +5,73 @@ public class PlayerController : MonoBehaviour
     Rigidbody2D rb;
     [SerializeField] float jumpPower;
     [SerializeField] float up;
-    //[SerializeField] float airMovement;
     [SerializeField] float groundCheckDistance;
     [SerializeField] LayerMask groundLayer;
 
-    bool isGrounded;
-    bool canDoubleJump;
-
-    void Start()
-    {
+   [SerializeField] bool isGrounded;
+   [SerializeField] bool canDoubleJump;
+  
+    void Start(){
         rb = GetComponent<Rigidbody2D>();
     }
 
-    void Update()
-    {
+    void Update(){
         isGrounded = Physics2D.Raycast(transform.position, Vector2.down, groundCheckDistance, groundLayer);
 
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            if (isGrounded)
-            {
+        if (Input.GetKeyDown(KeyCode.A) || LeftClick()){
+            if (isGrounded){
                 JumpPlayer(-jumpPower);
                 canDoubleJump = true;
             }
-            else if (canDoubleJump)
-            {
+            else if (canDoubleJump){
                 JumpPlayer(-jumpPower);
                 canDoubleJump = false;
             }
+            else if (!canDoubleJump && !isGrounded) rb.AddForce(Vector2.down * 8, ForceMode2D.Impulse);
             transform.eulerAngles = new Vector2(0, 180);
         }
-        else if (Input.GetKeyDown(KeyCode.D))
-        {
-            if (isGrounded)
-            {
-                JumpPlayer(jumpPower);
+        else if (Input.GetKeyDown(KeyCode.D) || RightClick()){
+            if (isGrounded){
+                JumpPlayer(jumpPower);  
                 canDoubleJump = true;
+
             }
-            else if (canDoubleJump)
-            {
-                JumpPlayer(jumpPower);
+            else if (canDoubleJump){
+                JumpPlayer(jumpPower);  
                 canDoubleJump = false;
+                
             }
+            else if (!canDoubleJump && !isGrounded) rb.AddForce(Vector2.down * 8, ForceMode2D.Impulse);
             transform.eulerAngles = new Vector2(0, 0);
         }
+       
+        //if (!canDoubleJump && !isGrounded && (Input.GetKeyDown(KeyCode.A) || LeftClick() || Input.GetKeyDown(KeyCode.D) || RightClick()))
+        //    rb.AddForce(Vector2.down * up, ForceMode2D.Impulse);
 
-        if (!isGrounded)
-        {
-            float horizontalInput = Input.GetAxis("Horizontal");
-            rb.AddForce(new Vector2(horizontalInput, 0), ForceMode2D.Force);
-        }
+
     }
+    
 
-    void JumpPlayer(float power)
-    {
+    void JumpPlayer(float power){
         rb.velocity = new Vector2(power, 0);
         rb.AddForce(Vector2.up * up, ForceMode2D.Impulse);
+        
+    }
+
+    bool LeftClick(){
+        if (Input.touchCount > 0){
+            Touch touch = Input.GetTouch(0);
+            return touch.phase == TouchPhase.Began && touch.position.x < Screen.width / 2;
+        }
+        return false;
+    }
+
+    bool RightClick(){
+        if (Input.touchCount > 0){
+            Touch touch = Input.GetTouch(0);
+            return touch.phase == TouchPhase.Began && touch.position.x > Screen.width / 2;
+        }
+        return false;
     }
 }
+
